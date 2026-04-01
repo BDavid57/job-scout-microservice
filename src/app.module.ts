c0b444de22bae69bf74c34901fd8f702a88f1d0a
David <davidbanciu57@gmail.com>
2026-04-01 17:01:25 +0300

    add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './libs/database/database.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'db',
-      port: 5432,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      autoLoadEntities: true,
-      synchronize: true, // ONLY dev
+    ConfigModule.forRoot({
+      isGlobal: true, 
     }),
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/libs/database/database.config.ts
@@ -0,0 +1,15 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const getDatabaseConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST', 'db'),
+  port: 5432,
+  username: configService.get<string>('POSTGRES_USER'),
+  password: configService.get<string>('POSTGRES_PASSWORD'),
+  database: configService.get<string>('POSTGRES_DB'),
+  autoLoadEntities: true,
+  synchronize: true,
+});
